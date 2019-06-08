ae2645c86f7febcf4fc2b60905a0cb4b7ca91204
Mark Estudillo <markestu@marks-mbp-3.home>
2019-06-08 19:34:24 -0400

    Survey

--- survey_creation.py
+++ survey_creation.py
@@ -3,11 +3,11 @@ import requests
 from credentials import * 
 
 # Setting user Parameters
-# apiToken = "mpqVbDZQGN9DBleFL0vm5X6g3AVmE3JLkULn7X3h" # Replace with Account API Token
-# dataCenter = "nyu.ca1"
+
 
 baseUrl = "https://{0}.qualtrics.com/API/v3/survey-definitions".format(
     dataCenter)
+
 headers = {
     "x-api-token": apiToken,
     "content-type": "application/json",
